aab556240324a1bcc17d86ab33320c6504916d9b
Hemant <hemant.shelar@gmail.com>
2024-02-04 22:34:00 +1100

    Login/Logout buttons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'twilio-deomo';
+  loggedIn = false;
+  public isLoggedIn(){
+    return this.loggedIn;
+  }
+
+  onLogInClicked(){
+    this.loggedIn = true;
+  }
+
+  onLogOutClicked(){
+    this.loggedIn = false;
+  }
+
 }
